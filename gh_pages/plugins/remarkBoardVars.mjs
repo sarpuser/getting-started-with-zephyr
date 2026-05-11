8ae10e08cb6660db33c4f34f49f5580ffbf0c626
Sarp User <23362324+sarpuser@users.noreply.github.com>
2026-05-11 16:26:18 -0400

    Fixed windows openocd path

--- gh_pages/plugins/remarkBoardVars.mjs
+++ gh_pages/plugins/remarkBoardVars.mjs
@@ -17,10 +17,10 @@ const boardMap = {
   },
   pic32bz6: {
     BOARD:            'pic32wm_bz6204_curiosity',
-    BOARD_NAME:       'PIC32WM BZ6 Curiosity',
+    BOARD_NAME:       'PIC32-BZ6 Curiosity',
     FLASH_ARGS_LINUX: '--openocd openOCD-wireless/prebuilt_binaries/linux/bin/openocd',
     FLASH_ARGS_MACOS: '--openocd openOCD-wireless/prebuilt_binaries/macos/bin/openocd',
-    FLASH_ARGS_WIN:   '--openocd openOCD-wireless/prebuilt_binaries/windows/openocd_support_wbz_pic32wm/bin/openocd.exe',
+    FLASH_ARGS_WIN:   '--openocd openOCD-wireless\\prebuilt_binaries\\windows\\openocd_support_wbz_pic32wm\\bin\\openocd.exe',
   },
 };
 
